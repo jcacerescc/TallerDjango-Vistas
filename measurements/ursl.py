5f1c392f17444aeec2057714fe1397a8eb1a4238
Sebastian C <j.caceresc@uniandes.edu.co>
2022-08-27 11:37:12 -0500

    fix error runserver

--- measurements/ursl.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlspatterns={
-    path('', views.measurements_view, name='measurements_view'),
-    path('<int:pk>', views.measurement_view, name='measurement_view'),
-}
