df4130da22584c4db5759319f197381ccb54fdd9
Sebastian C <j.caceresc@uniandes.edu.co>
2022-08-28 13:41:41 -0500

    Faltan postman

--- measurements/urls.py
+++ measurements/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-urlpatterns={
+urlpatterns=[
     path('', views.measurements_view, name='measurements_view'),
     path('<int:pk>', views.measurement_view, name='measurement_view'),
-}
+]
